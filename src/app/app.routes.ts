0bc02c47e1b9d99793a90c20e600b474080d1984
fran24 <arancibia.vega003@gmail.com>
2026-05-25 23:55:02 -0400

    Add welcome page with navigation buttons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,11 +7,15 @@ export const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'welcome',
     pathMatch: 'full',
   },
   {
     path: 'login',
     loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'welcome',
+    loadComponent: () => import('./welcome/welcome.page').then( m => m.WelcomePage)
   },
 ];
