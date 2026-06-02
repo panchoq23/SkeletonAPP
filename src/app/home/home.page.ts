6668ebff650d827ba6567de394b536662f84595b
fran24 <arancibia.vega003@gmail.com>
2026-06-01 22:37:22 -0400

    Add shake animation to inputs and implement Material DatePicker for date of birth

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonInput, IonButton, IonLabel, IonSelect, IonSelectOption, AlertController } from '@ionic/angular/standalone';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 import { UserService, UserData } from '../services/user.service';
 
 @Component({
@@ -23,16 +27,23 @@ import { UserService, UserData } from '../services/user.service';
     IonLabel,
     IonSelect,
     IonSelectOption,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
     CommonModule,
     FormsModule
   ],
 })
 export class HomePage implements OnInit {
+  @ViewChild('nombreInput') nombreInput!: ElementRef;
+  @ViewChild('apellidoInput') apellidoInput!: ElementRef;
+
   usuario: string = '';
   nombre: string = '';
   apellido: string = '';
   nivelEducacion: string = '';
-  fechaNacimiento: string = '';
+  fechaNacimiento: Date | null = null;
 
   constructor(
     private userService: UserService,
@@ -46,7 +57,15 @@ export class HomePage implements OnInit {
       this.nombre = userData.nombre || '';
       this.apellido = userData.apellido || '';
       this.nivelEducacion = userData.nivelEducacion || '';
-      this.fechaNacimiento = userData.fechaNacimiento || '';
+      
+      // Convertir string a Date si es necesario
+      if (userData.fechaNacimiento) {
+        if (typeof userData.fechaNacimiento === 'string') {
+          this.fechaNacimiento = new Date(userData.fechaNacimiento);
+        } else {
+          this.fechaNacimiento = userData.fechaNacimiento as Date;
+        }
+      }
     }
   }
 
@@ -54,7 +73,30 @@ export class HomePage implements OnInit {
     this.nombre = '';
     this.apellido = '';
     this.nivelEducacion = '';
-    this.fechaNacimiento = '';
+    this.fechaNacimiento = null;
+
+    // Aplicar animación shake a los inputs de nombre y apellido
+    this.aplicarAnimacionShake();
+  }
+
+  aplicarAnimacionShake() {
+    // Agregar clase de animación al input de nombre
+    const nombreEl = this.nombreInput?.nativeElement;
+    if (nombreEl) {
+      nombreEl.classList.add('shake-animation');
+      setTimeout(() => {
+        nombreEl.classList.remove('shake-animation');
+      }, 1000);
+    }
+
+    // Agregar clase de animación al input de apellido
+    const apellidoEl = this.apellidoInput?.nativeElement;
+    if (apellidoEl) {
+      apellidoEl.classList.add('shake-animation');
+      setTimeout(() => {
+        apellidoEl.classList.remove('shake-animation');
+      }, 1000);
+    }
   }
 
   async mostrar() {
@@ -69,13 +111,20 @@ export class HomePage implements OnInit {
       return;
     }
 
+    // Formatear fecha
+    let fechaFormato = '';
+    if (this.fechaNacimiento) {
+      const fecha = new Date(this.fechaNacimiento);
+      fechaFormato = fecha.toLocaleDateString('es-ES');
+    }
+
     // Guardar datos en el servicio
     this.userService.setUserData({
       usuario: this.usuario,
       nombre: this.nombre,
       apellido: this.apellido,
       nivelEducacion: this.nivelEducacion,
-      fechaNacimiento: this.fechaNacimiento
+      fechaNacimiento: fechaFormato
     });
 
     // Mostrar alerta con la información
@@ -87,8 +136,8 @@ export class HomePage implements OnInit {
     if (this.nivelEducacion) {
       mensaje += `<br><strong>Nivel de Educación:</strong> ${this.nivelEducacion}`;
     }
-    if (this.fechaNacimiento) {
-      mensaje += `<br><strong>Fecha de Nacimiento:</strong> ${this.fechaNacimiento}`;
+    if (fechaFormato) {
+      mensaje += `<br><strong>Fecha de Nacimiento:</strong> ${fechaFormato}`;
     }
 
     const alert = await this.alertController.create({
