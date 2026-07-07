f6cf4845924b5851e0e39d0327587c30204afc06
fran24 <arancibia.vega003@gmail.com>
2026-07-06 22:34:04 -0400

    Fix: Restauración de sesión activa y actualización dinámica del perfil en el menú

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,63 @@
 ﻿import { Component, inject, OnInit } from "@angular/core";
-import { IonApp, IonRouterOutlet, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonIcon, IonLabel, IonMenuToggle, NavController } from "@ionic/angular/standalone";
+import { CommonModule } from "@angular/common";
+import {
+  IonApp, IonRouterOutlet, IonMenu, IonHeader, IonToolbar, IonTitle,
+  IonContent, IonList, IonItem, IonIcon, IonLabel, IonMenuToggle,
+  NavController
+} from "@ionic/angular/standalone";
+import { RouterLinkActive, RouterLink } from "@angular/router";
 import { addIcons } from "ionicons";
-import { 
-  homeOutline, logOutOutline, listOutline, personOutline, 
-  mapOutline, cameraOutline, addCircleOutline 
+import {
+  homeOutline, logOutOutline, listOutline, personOutline,
+  mapOutline, cameraOutline, addCircleOutline,
+  businessOutline
 } from "ionicons/icons";
-import { AuthService } from "./services/auth.service";
+import { AuthService }   from "./services/auth.service";
 import { DBTaskService } from "./services/dbtask.service";
-import { Router } from "@angular/router";
+import { UserService }   from "./services/user.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
   standalone: true,
-  imports: [IonApp, IonRouterOutlet, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonIcon, IonLabel, IonMenuToggle],
+  imports: [
+    CommonModule, RouterLinkActive, RouterLink,
+    IonApp, IonRouterOutlet, IonMenu, IonHeader, IonToolbar, IonTitle,
+    IonContent, IonList, IonItem, IonIcon, IonLabel, IonMenuToggle,
+  ],
 })
 export class AppComponent implements OnInit {
-  private readonly authService = inject(AuthService);
-  private readonly dbService = inject(DBTaskService);
-  private readonly navCtrl = inject(NavController);
-  private readonly router = inject(Router);
+  private readonly authService  = inject(AuthService);
+  private readonly dbService    = inject(DBTaskService);
+  private readonly userService  = inject(UserService);
+  private readonly navCtrl      = inject(NavController);
+
+  usuario: string = "";
 
   constructor() {
-    addIcons({ 
-      homeOutline, logOutOutline, listOutline, personOutline, 
-      mapOutline, cameraOutline, addCircleOutline 
+    addIcons({
+      homeOutline, logOutOutline, listOutline, personOutline,
+      mapOutline, cameraOutline, addCircleOutline, businessOutline
     });
   }
 
   async ngOnInit() {
     await this.dbService.initializePlugin();
+
+    const session = await this.dbService.isSessionActive();
+    if (session) {
+      const dbUser = await this.dbService.getUser(session.user_name);
+      this.userService.setUserData(dbUser ?? { usuario: session.user_name });
+    }
+
+    this.userService.userData$.subscribe(u => {
+      this.usuario = u?.nombre || u?.usuario || "";
+    });
   }
 
   goToProfile() {
-    this.router.navigate(["/home"], { queryParams: { segment: "perfil" } });
+    this.navCtrl.navigateForward("/home", { queryParams: { segment: "perfil" } });
   }
 
   async logout() {
